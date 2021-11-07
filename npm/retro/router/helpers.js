bfef723138e11fb6e438b1a300fe425726997975
Zaydek Michels-Gualtieri <zaydek@Zaydeks-MacBook-Pro.local>
2021-11-06 22:47:48 -0700

    v0.1.0-alpha.39: Experimental support for process.env.LOCATION

--- npm/retro/router/helpers.js
+++ npm/retro/router/helpers.js
@@ -1,7 +1,7 @@
 export function getCurrentPathSSR() {
 	let path = "/"
-	if (typeof window !== "undefined") {
-		path = window.location.pathname
+	if (typeof process.env.LOCATION !== "undefined") {
+		path = process.env.LOCATION
 	}
 	if (path.endsWith(".html")) {
 		path = path.slice(0, -".html".length)
