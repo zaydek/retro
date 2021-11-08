5e09db8e472f73519d598a2cba78668dbaf80780
Zaydek Michels-Gualtieri <zaydek@Zaydeks-MacBook-Pro.local>
2021-11-07 21:16:46 -0800

    v0.1.0-alpha.44: Expose '<DocumentTitle>' components

--- go/cmd/create_retro_app/static/retro.config.js
+++ go/cmd/create_retro_app/static/retro.config.js
@@ -1,5 +1,8 @@
 // https://esbuild.github.io/api/#build-api
 
+/**
+ * @type { import("esbuild").BuildOptions }
+ */
 module.exports = {
 	target: ["es2017"],
 }
--- retro.config.js
+++ retro.config.js
@@ -1,8 +1,12 @@
 // https://esbuild.github.io/api/#build-api
 
+/**
+ * @type { import("esbuild").BuildOptions }
+ */
 module.exports = {
 	plugins: [
 		require("./npm/retro/plugins/mdx"),
 		require("./npm/retro/plugins/sass"),
 	],
+	target: ["es2017"],
 }
